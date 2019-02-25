6f3627dd3d3a6ad8ebd9b780c84c3f41c5c64df7
peperelo <pepito_481@msn.com>
2019-02-25 08:34:45 -0600

    Modifica el codigo para que siga el estandar pep8

--- Calculadora.py
+++ Calculadora.py
@@ -1,6 +1,6 @@
 
 
 class Calculadora(object):
-    
+
     def sumar(self, num1, num2):
         return num1 + num2
--- TestCalculadora.py
+++ TestCalculadora.py
@@ -2,19 +2,20 @@ import unittest
 
 from Calculadora import Calculadora
 
+
 class TestCalculadora(unittest.TestCase):
 
     def test_suma_2_mas_2(self):
         calc = Calculadora()
-        self.assertEquals(4, calc.sumar(2,2))
-    
+        self.assertEquals(4, calc.sumar(2, 2))
+
     def test_suma_5_mas_10(self):
         calc = Calculadora()
-        self.assertEquals(15, calc.sumar(5,10))
-    
+        self.assertEquals(15, calc.sumar(5, 10))
+
     def test_suma_menos5_mas_10(self):
         calc = Calculadora()
-        self.assertEquals(5, calc.sumar(-5,10))
+        self.assertEquals(5, calc.sumar(-5, 10))
 
 
 if __name__ == "__main__":
